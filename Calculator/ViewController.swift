da7c0c2592ba7b7b4e2c964979c380bf40fbdc31
yusheng593 <54464265+yusheng593@users.noreply.github.com>
2022-10-03 18:01:43 +0800

    計算機能累積數字

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,18 +12,44 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+        
+        switch sender.currentTitle {
+        case "AC":
+            displayLabel.text = "0"
+        default:
+            break
+        }
+        
+        
     
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else if displayLabel.text == "0" {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+            
+            
+            
+        }
+        
+        
     
     }
 
